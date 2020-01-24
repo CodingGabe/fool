335b2311d4ddfc7d5503a934f4c016bc0a446264
Gabe Amaya <GabeAmaya2@gmail.com>
2020-01-23 18:40:36 -0600

    add sass, cleanup code

---
+++ static/script.js
@@ -0,0 +1,10 @@
+fetch('articles.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
